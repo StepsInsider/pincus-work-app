4bc5c2f10507e2c18ef57fa01c1c4e1e521f8134
Ben <sparenberg2000@gmail.com>
2026-08-12 00:04:34 +0200

    fix(ci): fix Flutter installation command

--- scripts/netlify-build.sh
+++ scripts/netlify-build.sh
@@ -7,13 +7,8 @@ FLUTTER_VERSION="3.44.4"
 FLUTTER_DIR="$HOME/flutter"
 
 if [ ! -x "$FLUTTER_DIR/bin/flutter" ]; then
-  echo "=== INSTALLIERE FLUTTER ${FLUTTER_VERSION} ==="
-
-  git clone \
-    --depth 1 \
-    --branch "$FLUTTER_VERSION" \
-    https://github.com/flutter/flutter.git \
-    "$FLUTTER_DIR"
+    echo "=== INSTALLIERE FLUTTER ${FLUTTER_VERSION} ==="
+    git clone --depth 1 --branch "$FLUTTER_VERSION" https://github.com/flutter/flutter.git "$FLUTTER_DIR"
 fi
 
 export PATH="$FLUTTER_DIR/bin:$PATH"
