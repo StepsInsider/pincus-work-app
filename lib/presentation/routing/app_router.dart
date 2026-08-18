f9dc3e61fa841bce8ca2ef8fa9b64734bf78f896
Stepsinsider <119820978+StepsInsider@users.noreply.github.com>
2026-08-18 22:54:02 +0200

    feat: add leads route

--- lib/presentation/routing/app_router.dart
+++ lib/presentation/routing/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 
 import '../../screens/dashboard_screen.dart';
+import '../../screens/leads_screen.dart';
 import '../../features/jokes/presentation/joke_page.dart';
 
 final appRouter = GoRouter(
@@ -9,6 +10,10 @@ final appRouter = GoRouter(
     GoRoute(
       path: '/',
       builder: (context, state) => const DashboardScreen(),
+    ),
+    GoRoute(
+      path: '/leads',
+      builder: (context, state) => const LeadsScreen(),
     ),
     GoRoute(
       path: '/joke',
