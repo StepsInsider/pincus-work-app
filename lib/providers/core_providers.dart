c21f2a209c90206691dcb376167ccb7c95790588
Stepsinsider <119820978+StepsInsider@users.noreply.github.com>
2026-09-13 00:33:41 +0200

    feat: add location providers

--- lib/providers/core_providers.dart
+++ lib/providers/core_providers.dart
@@ -4,66 +4,28 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import '../models/core_baustelle.dart';
 import '../models/core_customer.dart';
 import '../models/core_employee.dart';
+import '../models/core_location.dart';
 import '../models/core_order.dart';
 import '../models/core_time_entry.dart';
 import '../repositories/core_repositories.dart';
 
-final coreSupabaseClientProvider = Provider<SupabaseClient>((ref) {
-  return Supabase.instance.client;
-});
+final coreSupabaseClientProvider = Provider<SupabaseClient>((ref) => Supabase.instance.client);
 
-final coreCustomerRepositoryProvider = Provider<CoreCustomerRepository>((ref) {
-  return CoreCustomerRepository(ref.watch(coreSupabaseClientProvider));
-});
+final coreCustomerRepositoryProvider = Provider<CoreCustomerRepository>((ref) => CoreCustomerRepository(ref.watch(coreSupabaseClientProvider)));
+final coreLocationRepositoryProvider = Provider<CoreLocationRepository>((ref) => CoreLocationRepository(ref.watch(coreSupabaseClientProvider)));
+final coreBaustelleRepositoryProvider = Provider<CoreBaustelleRepository>((ref) => CoreBaustelleRepository(ref.watch(coreSupabaseClientProvider)));
+final coreEmployeeRepositoryProvider = Provider<CoreEmployeeRepository>((ref) => CoreEmployeeRepository(ref.watch(coreSupabaseClientProvider)));
+final coreOrderRepositoryProvider = Provider<CoreOrderRepository>((ref) => CoreOrderRepository(ref.watch(coreSupabaseClientProvider)));
+final coreTimeEntryRepositoryProvider = Provider<CoreTimeEntryRepository>((ref) => CoreTimeEntryRepository(ref.watch(coreSupabaseClientProvider)));
 
-final coreBaustelleRepositoryProvider = Provider<CoreBaustelleRepository>((ref) {
-  return CoreBaustelleRepository(ref.watch(coreSupabaseClientProvider));
-});
-
-final coreEmployeeRepositoryProvider = Provider<CoreEmployeeRepository>((ref) {
-  return CoreEmployeeRepository(ref.watch(coreSupabaseClientProvider));
-});
-
-final coreOrderRepositoryProvider = Provider<CoreOrderRepository>((ref) {
-  return CoreOrderRepository(ref.watch(coreSupabaseClientProvider));
-});
-
-final coreTimeEntryRepositoryProvider = Provider<CoreTimeEntryRepository>((ref) {
-  return CoreTimeEntryRepository(ref.watch(coreSupabaseClientProvider));
-});
-
-final coreCustomersProvider = FutureProvider<List<CoreCustomer>>((ref) {
-  return ref.watch(coreCustomerRepositoryProvider).list();
-});
-
-final coreBaustellenProvider = FutureProvider<List<CoreBaustelle>>((ref) {
-  return ref.watch(coreBaustelleRepositoryProvider).list();
-});
-
-final coreEmployeesProvider = FutureProvider<List<CoreEmployee>>((ref) {
-  return ref.watch(coreEmployeeRepositoryProvider).list();
-});
-
-final coreOrdersProvider = FutureProvider<List<CoreOrder>>((ref) {
-  return ref.watch(coreOrderRepositoryProvider).list();
-});
-
-final coreOrdersByCustomerProvider = FutureProvider.family<List<CoreOrder>, String>((ref, customerId) {
-  return ref.watch(coreOrderRepositoryProvider).list(customerId: customerId);
-});
-
-final coreOrdersByBaustelleProvider = FutureProvider.family<List<CoreOrder>, String>((ref, siteId) {
-  return ref.watch(coreOrderRepositoryProvider).list(siteId: siteId);
-});
-
-final coreTimeEntriesProvider = FutureProvider<List<CoreTimeEntry>>((ref) {
-  return ref.watch(coreTimeEntryRepositoryProvider).list();
-});
-
-final coreTimeEntriesByEmployeeProvider = FutureProvider.family<List<CoreTimeEntry>, String>((ref, employeeId) {
-  return ref.watch(coreTimeEntryRepositoryProvider).list(employeeId: employeeId);
-});
-
-final coreTimeEntriesByBaustelleProvider = FutureProvider.family<List<CoreTimeEntry>, String>((ref, siteId) {
-  return ref.watch(coreTimeEntryRepositoryProvider).list(siteId: siteId);
-});
+final coreCustomersProvider = FutureProvider<List<CoreCustomer>>((ref) => ref.watch(coreCustomerRepositoryProvider).list());
+final coreLocationsProvider = FutureProvider<List<CoreLocation>>((ref) => ref.watch(coreLocationRepositoryProvider).list());
+final coreLocationsByCustomerProvider = FutureProvider.family<List<CoreLocation>, String>((ref, customerId) => ref.watch(coreLocationRepositoryProvider).list(customerId: customerId));
+final coreBaustellenProvider = FutureProvider<List<CoreBaustelle>>((ref) => ref.watch(coreBaustelleRepositoryProvider).list());
+final coreEmployeesProvider = FutureProvider<List<CoreEmployee>>((ref) => ref.watch(coreEmployeeRepositoryProvider).list());
+final coreOrdersProvider = FutureProvider<List<CoreOrder>>((ref) => ref.watch(coreOrderRepositoryProvider).list());
+final coreOrdersByCustomerProvider = FutureProvider.family<List<CoreOrder>, String>((ref, customerId) => ref.watch(coreOrderRepositoryProvider).list(customerId: customerId));
+final coreOrdersByBaustelleProvider = FutureProvider.family<List<CoreOrder>, String>((ref, siteId) => ref.watch(coreOrderRepositoryProvider).list(siteId: siteId));
+final coreTimeEntriesProvider = FutureProvider<List<CoreTimeEntry>>((ref) => ref.watch(coreTimeEntryRepositoryProvider).list());
+final coreTimeEntriesByEmployeeProvider = FutureProvider.family<List<CoreTimeEntry>, String>((ref, employeeId) => ref.watch(coreTimeEntryRepositoryProvider).list(employeeId: employeeId));
+final coreTimeEntriesByBaustelleProvider = FutureProvider.family<List<CoreTimeEntry>, String>((ref, siteId) => ref.watch(coreTimeEntryRepositoryProvider).list(siteId: siteId));
